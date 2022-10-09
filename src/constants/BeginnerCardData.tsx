f4e201b01fc328cda0eb587a3529a2d83811e367
Kyle Trammell <ceotrammell@icloud.com>
2022-10-09 01:37:43 -0500

    redesign beginnersectioncards for mobile and desktop 3x col

--- src/constants/BeginnerCardData.tsx
+++ src/constants/BeginnerCardData.tsx
@@ -23,5 +23,74 @@ export const BeginnerCardData:
         ],
         link: 'https://cs.txstate.edu/',
     },
+    {
+        title: 'Rate My Professor',
+        sub_title: 'Reviews of professors',
+        li_items: [
+            'see who is good',
+            ' see who is bad'
+        ],
+        link: 'https://www.ratemyprofessors.com/search/teachers?query=*&sid=938',
+    },
+    {
+        title: 'HB 2504',
+        sub_title: 'More reviews of professors',
+        li_items: [
+            'see TXST student surveys',
+            'see past syllabi for classes'
+        ],
+        link: 'BLAH',
+    },
+    {
+        title: 'Harvard CS50',
+        sub_title: 'free Harvard lecture videos',
+        li_items: [
+            'learn CS basics',
+            'learn coding basics'
+        ],
+        link: 'https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0',
+    },
+    {
+        title: 'Crash Course',
+        sub_title: 'fun animated videos',
+        li_items: [
+            'learn CS concepts',
+            'learn CS history'
+        ],
+        link: 'https://www.youtube.com/playlist?list=PLME-KWdxI8dcaHSzzRsNuOLXtM2Ep_C7a',
+    },
+    {
+        title: 'TXST Linux Tutorial',
+        sub_title: 'linux basics',
+        li_items: [
+            'learn how to use free linux servers at TXST',
+        ],
+        link: 'https://canvas.txstate.edu/courses/1697110',
+    },
+    {
+        title: 'TXST flowchart',
+        sub_title: 'visual degree path',
+        li_items: [
+            'see what classes you need',
+        ],
+        link: 'https://www.cose.txstate.edu/advising/cose-majors-and-minors/flowcharts.html',
+    },
+    {
+        title: 'Google Tech Guide',
+        sub_title: 'free interactive tutorials',
+        li_items: [
+            ' designed by Google to get you a job at Google',
+        ],
+        link: 'https://techdevguide.withgoogle.com/',
+    },
+    {
+        title: 'LinkedIn Learning',
+        sub_title: 'free video tutorials',
+        li_items: [
+            'earn certifications',
+            '(log in with netID)'
+        ],
+        link: 'https://www.linkedin.com/learning/',
+    },
 ];
 
