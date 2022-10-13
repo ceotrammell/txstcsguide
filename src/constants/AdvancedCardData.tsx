b883f1cee1594e91520ff5dd33e1dc5ee9a4d842
Kyle Trammell <ceotrammell@icloud.com>
2022-10-12 23:35:56 -0500

    advancedcarddata finish data entry

--- src/constants/AdvancedCardData.tsx
+++ src/constants/AdvancedCardData.tsx
@@ -15,22 +15,280 @@ export const AdvancedCardData:
     }> = [
     {
         en: {
-            title: 'BLAH',
-            sub_title: 'BLAH',
+            title: 'Google Tech Guide',
+            sub_title: 'free interactive tutorials',
             li_items: [
-                'BLAH',
-                'BLAH'
+                'designed by Google to get you a job at Google',
             ],
-            link: 'BLAH',
+            link: 'https://techdevguide.withgoogle.com/',
         },
         es: {
-            title: 'BLAH',
-            sub_title: 'BLAH',
+            title: 'Guía técnica de Google',
+            sub_title: 'tutoriales interactivos gratuitos',
             li_items: [
-                'BLAH',
-                'BLAH'
+                'diseñado por Google para conseguirte un trabajo en Google',
             ],
-            link: 'BLAH',
+            link: 'https://techdevguide.withgoogle.com/',
+        },
+    },
+    {
+        en: {
+            title: 'LinkedIn Learning',
+            sub_title: 'free video tutorials',
+            li_items: [
+                'earn certifications',
+                '(log in with netID)'
+            ],
+            link: 'https://www.linkedin.com/learning/',
+        },
+        es: {
+            title: 'Aprendizaje de LinkedIn',
+            sub_title: 'tutoriales en vídeo gratuitos',
+            li_items: [
+                'obtener certificaciones',
+                '(inicie sesión con netID)'
+            ],
+            link: 'https://www.linkedin.com/learning/',
+        },
+    },
+    {
+        en: {
+            title: 'Google Cloud',
+            sub_title: 'interactive tutorials',
+            li_items: [
+                'learn about Cloud, AI/ML, Infrastructure, Security, and others',
+            ],
+            link: 'https://cloud.google.com/training',
+        },
+        es: {
+            title: 'Nube de Google',
+            sub_title: 'tutoriales interactivos',
+            li_items: [
+                'más información sobre cloud, AI/ML, infraestructura, seguridad y otros',
+            ],
+            link: 'https://cloud.google.com/training',
+        },
+    },
+    {
+        en: {
+            title: 'Kaggle',
+            sub_title: 'free data science tutorials',
+            li_items: [
+                'learn data analysis, SQL, AI',
+                'learn Machine Learning',
+                'use free datasets',
+            ],
+            link: 'https://www.kaggle.com/learn',
+        },
+        es: {
+            title: 'Kaggle',
+            sub_title: 'tutoriales gratuitos de ciencia de datos',
+            li_items: [
+                'aprender análisis de datos, SQL, AI',
+                'aprender Machine Learning',
+                'utilizar conjuntos de datos gratuitos',
+            ],
+            link: 'https://www.kaggle.com/learn',
+        },
+    },
+    {
+        en: {
+            title: 'SQL Bolt',
+            sub_title: 'free interactive tutorial',
+            li_items: [
+                'learn SQL',
+                'learn databases'
+            ],
+            link: 'https://sqlbolt.com/',
+        },
+        es: {
+            title: 'SQL Bolt',
+            sub_title: 'tutorial interactivo gratuito',
+            li_items: [
+                'aprender SQL',
+                'aprender bases de datos'
+            ],
+            link: 'https://sqlbolt.com/',
+        },
+    },
+    {
+        en: {
+            title: 'Odin Project',
+            sub_title: 'free full-stack tutorials',
+            li_items: [
+                'learn web dev',
+                'make resume projects'
+            ],
+            link: 'https://www.theodinproject.com/',
+        },
+        es: {
+            title: 'Proyecto Odín',
+            sub_title: 'tutoriales gratuitos de pila completa',
+            li_items: [
+                'aprender desarrollo web',
+                'hacer proyectos de currículum'
+            ],
+            link: 'https://www.theodinproject.com/',
+        },
+    },
+    {
+        en: {
+            title: 'CIE',
+            sub_title: 'for TXST entrepreneurs',
+            li_items: [
+                'learn startup skills',
+                'learn business skills',
+                'find a co-founder',
+            ],
+            link: 'https://innovation.txstate.edu/',
+        },
+        es: {
+            title: 'CIE',
+            sub_title: 'para emprendedores de TXST',
+            li_items: [
+                'aprender habilidades de inicio',
+                'aprender habilidades empresariales',
+                'encontrar un cofundador',
+            ],
+            link: 'https://innovation.txstate.edu/',
+        },
+    },
+    {
+        en: {
+            title: 'Open Source',
+            sub_title: 'tutorials for open source',
+            li_items: [
+                'learn how to contribute',
+                'make your first pull request'
+            ],
+            link: 'https://www.firsttimersonly.com/',
+        },
+        es: {
+            title: 'Código abierto',
+            sub_title: 'tutoriales para código abierto',
+            li_items: [
+                'aprende a contribuir',
+                'realice su primera solicitud de extracción'
+            ],
+            link: 'https://www.firsttimersonly.com/',
+        },
+    },
+    {
+        en: {
+            title: 'Leetcode',
+            sub_title: 'learn and compete',
+            li_items: [
+                'learn data structures',
+                'learn algorithms'
+            ],
+            link: 'http://www.txstcsstudentguide.com/advanced.html#https://leetcode.com/problemset/all/',
+        },
+        es: {
+            title: 'Leetcode',
+            sub_title: 'aprender y competir',
+            li_items: [
+                'aprender estructuras de datos',
+                'aprender algoritmos'
+            ],
+            link: 'http://www.txstcsstudentguide.com/advanced.html#https://leetcode.com/problemset/all/',
+        },
+    },
+    {
+        en: {
+            title: 'Grokking the Coding Interview',
+            sub_title: 'learn interview patterns',
+            li_items: [
+                '17 patterns',
+                'interactive tutorial',
+                '(not free)',
+            ],
+            link: 'https://www.educative.io/courses/grokking-the-coding-interview',
+        },
+        es: {
+            title: 'Superando la entrevista de codificación',
+            sub_title: 'aprender patrones de entrevista',
+            li_items: [
+                '17 patrones',
+                'tutorial interactivo',
+                '(no es gratis)',
+            ],
+            link: 'https://www.educative.io/courses/grokking-the-coding-interview',
+        },
+    },
+    {
+        en: {
+            title: 'Cracking the Coding Interview',
+            sub_title: 'book on technical interviewing',
+            li_items: [
+                'recommended by industry',
+                '(not free)'
+            ],
+            link: 'https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850',
+        },
+        es: {
+            title: 'Descifrando la entrevista de codificación',
+            sub_title: 'libro sobre entrevistas técnicas',
+            li_items: [
+                'recomendado por la industria',
+                '(no es gratis)',
+            ],
+            link: 'https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850',
+        },
+    },
+    {
+        en: {
+            title: 'Princeton Algorithms',
+            sub_title: 'free class in Java',
+            li_items: [
+                'learn algorithms',
+            ],
+            link: 'https://www.coursera.org/learn/algorithms-part1',
+        },
+        es: {
+            title: 'Algoritmos de Princeton',
+            sub_title: 'clase gratuita en Java',
+            li_items: [
+                'aprender algoritmos',
+            ],
+            link: 'https://www.coursera.org/learn/algorithms-part1',
+        },
+    },
+    {
+        en: {
+            title: 'MIT Algorithms',
+            sub_title: 'free class in Python',
+            li_items: [
+                'learn algorithms',
+                'includes notes, hw, exams, and solutions'
+            ],
+            link: 'https://ocw.mit.edu/courses/6-006-introduction-to-algorithms-spring-2020/',
+        },
+        es: {
+            title: 'Algoritmos MIT',
+            sub_title: 'clase gratuita en Python',
+            li_items: [
+                'aprender algoritmos',
+                'incluye notas, hw, exámenes y soluciones'
+            ],
+            link: 'https://ocw.mit.edu/courses/6-006-introduction-to-algorithms-spring-2020/',
+        },
+    },
+    {
+        en: {
+            title: 'MIT Missing Semester',
+            sub_title: 'free videos of useful topics',
+            li_items: [
+                'learn Git, debugging, shell, and others',
+            ],
+            link: 'https://missing.csail.mit.edu/',
+        },
+        es: {
+            title: 'Mit Semestre Perdido',
+            sub_title: 'videos gratuitos de temas útiles',
+            li_items: [
+                'aprender Git, depuración, shell y otros',
+            ],
+            link: 'https://missing.csail.mit.edu/',
         },
     },
 ];
