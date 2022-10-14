ffc71bdacd409baea36460b13f5585caa0989060
Kyle Trammell <ceotrammell@icloud.com>
2022-10-13 21:27:23 -0500

    finish club and job sestionsources and data

--- src/components/Jobs/JobsMastHead.tsx
+++ src/components/Jobs/JobsMastHead.tsx
@@ -7,8 +7,8 @@ function JobsMastHead() {
   return (
       <Container fluid className="max-width px-5 my-5">
          <div className="text-center mb-5">
-            <h1 className="fw-bolder display-3">{t("BEGINNER.MAST_HEAD.TITLE")}</h1>
-            <p className="lead fw-normal text-muted mb-0">{t("BEGINNER.MAST_HEAD.SUB_TITLE")}</p>
+            <h1 className="fw-bolder display-3">{t("JOBS.MAST_HEAD.TITLE")}</h1>
+            <p className="lead fw-normal text-muted mb-0">{t("JOBS.MAST_HEAD.SUB_TITLE")}</p>
         </div>
       </Container>
   );
