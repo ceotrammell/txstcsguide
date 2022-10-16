93aec744e7a348a8b5a2aec454437c6d260dc937
Kyle Trammell <ceotrammell@icloud.com>
2022-10-15 23:58:23 -0500

    remove express and rebuild package-lock, remove not needed imports

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,8 @@
-import Container from 'react-bootstrap/Container';
 import { useTranslation } from "react-i18next";
 import HomeMastHead from '../../components/Home/HomeMastHead';
 import HomeSectionResources from '../../components/Home/HomeSectionResources';
 
 function Home() {
-    const { t } = useTranslation();
 
   return (
       <>
